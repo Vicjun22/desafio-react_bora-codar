1d0240cde7e622ae728f1ccbc091e10b7068e860
Victor Ross <vicjun22@gmail.com>
2023-04-28 16:56:18 -0300

    Update Home Page Chat

--- chat-iterativo/src/ui/screens/setName/home.screen.jsx
+++ chat-iterativo/src/ui/screens/setName/home.screen.jsx
@@ -1,24 +1,59 @@
-import "./home.style.css";
-
-import { useState } from "react";
+import './home.style.css';
+import { useState } from 'react';
 
 export function Home() {
-    const [myName, setMyName] = useState("");
-    const [friendsName, setFriendsName] = useState("");
+  const [myName, setMyName] = useState('');
+  const [friendsName, setFriendsName] = useState('');
 
-    return (
-        <>
-            <form>
-                <div>
-                    <label>Digite seu nome:</label>
-                    <input type="text" value={myName} onChange={(e) => setMyName(e.target.value)} ></input>
-                </div>
-                <div>
-                    <label>Digite o nome de seu amigo:</label>
-                    <input type="text" value={friendsName} onChange={(e) => setFriendsName(e.target.value)} ></input>
-                </div>
-            </form>
-            <button onSubmit={() => handleSubmit()}>Iniciar Chat</button>
-        </>
-    )
+  const handleInputFocus = (event) => {
+    event.target.placeholder = '';
+    event.target.value = '';
+  };
+
+  const handleInputBlur = (event, placeholder) => {
+    if (!event.target.value) {
+      event.target.placeholder = placeholder;
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Faz alguma ação quando o formulário é enviado
+  };
+
+  return (
+    <form className="home-form" onSubmit={handleSubmit}>
+      <label className="home-label">
+        Digite seu nome:
+        <input
+          className="home-input"
+          type="text"
+          value={myName}
+          placeholder="Digite seu nome aqui"
+          onFocus={handleInputFocus}
+          onBlur={(event) =>
+            handleInputBlur(event, 'Digite seu nome aqui')
+          }
+          onChange={(e) => setMyName(e.target.value)}
+        />
+      </label>
+      <label className="home-label">
+        Digite o nome de seu amigo:
+        <input
+          className="home-input"
+          type="text"
+          value={friendsName}
+          placeholder="Digite o nome de seu amigo aqui"
+          onFocus={handleInputFocus}
+          onBlur={(event) =>
+            handleInputBlur(event, 'Digite o nome de seu amigo aqui')
+          }
+          onChange={(e) => setFriendsName(e.target.value)}
+        />
+      </label>
+      <button className="home-btn" type="submit">
+        Iniciar Chat
+      </button>
+    </form>
+  );
 }
